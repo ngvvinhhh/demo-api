bb22247dba8859c0f1806698c75a0574c5c052aa
Nguyen Duc Quy <ducquyyy@gmail.com>
2025-03-24 10:37:21 +0700

    update chat

--- src/main/java/com/example/demo/api/ChatController.java
+++ src/main/java/com/example/demo/api/ChatController.java
@@ -2,6 +2,7 @@ package com.example.demo.api;
 
 import com.example.demo.entity.Chat;
 import com.example.demo.service.ChatService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/chats")
+@RequestMapping("/chats")
+@SecurityRequirement(name = "api")
+@CrossOrigin("*")
 public class ChatController {
 
     @Autowired
